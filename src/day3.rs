7df87e1e52a807539de12d428d06a7189788dfa9
Avataw <avataw.dev@gmail.com@>
2023-01-05 17:40:22 +0100

    add day four

---
+++ src/day3.rs
@@ -0,0 +1,181 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
+
+const PATH: &str = "src/day3.txt";
+
+pub fn solve() -> () {
+    let input: String = fs::read_to_string(PATH).expect("");
+    println!(
+        "Day 3 a) : {} b) : {}",
+        solve_part_one(&input),
+        solve_part_two(&input)
+    );
+}
+
+fn solve_part_one(input: &str) -> i32 {
+    let mut wires: Vec<Wire> = vec![];
+
+    for line in input.lines() {
+        let mut wire = Wire::new();
+
+        let movements = line.split(",");
+
+        for movement in movements {
+            let value = movement[1..movement.len()].parse::<i32>().unwrap();
+
+            for _ in 0..value {
+                if movement.starts_with("R") {
+                    wire.move_right();
+                } else if movement.starts_with("L") {
+                    wire.move_left();
+                } else if movement.starts_with("U") {
+                    wire.move_up()
+                } else if movement.starts_with("D") {
+                    wire.move_down()
+                }
+            }
+        }
+
+        wires.push(wire)
+    }
+
+    let first_visited: HashSet<Position> = wires[0].visited_at.keys().cloned().collect();
+    let second_visited: HashSet<Position> = wires[1].visited_at.keys().cloned().collect();
+
+    let intersections: Vec<&Position> = first_visited.intersection(&second_visited).collect();
+    
+    let center_point = Position { x: 0, y: 0 };
+    
+    let mut intersections_distances: Vec<i32> = intersections
+        .into_iter()
+        .map(|i| i.distance_from(&center_point))
+        .collect();
+
+    intersections_distances.sort();
+
+    intersections_distances[0]
+}
+
+fn solve_part_two(input: &str) -> i32 {
+    let mut wires: Vec<Wire> = vec![];
+
+    for line in input.lines() {
+        let mut wire = Wire::new();
+
+        let movements = line.split(",");
+
+        for movement in movements {
+            let value = movement[1..movement.len()].parse::<i32>().unwrap();
+
+            for _ in 0..value {
+                if movement.starts_with("R") {
+                    wire.move_right();
+                } else if movement.starts_with("L") {
+                    wire.move_left();
+                } else if movement.starts_with("U") {
+                    wire.move_up()
+                } else if movement.starts_with("D") {
+                    wire.move_down()
+                }
+            }
+        }
+
+        wires.push(wire)
+    }
+
+    let first_visited: HashSet<Position> = wires[0].visited_at.keys().cloned().collect();
+    let second_visited: HashSet<Position> = wires[1].visited_at.keys().cloned().collect();
+
+    let intersections: Vec<&Position> = first_visited.intersection(&second_visited).collect();
+
+    let mut intersections_distances: Vec<i32> = intersections
+        .into_iter()
+        .map(|i| wires[0].visited_at.get(i).unwrap() + wires[1].visited_at.get(i).unwrap())
+        .collect();
+
+    intersections_distances.sort();
+
+    intersections_distances[0]
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+impl Position {
+    fn distance_from(&self, other: &Position) -> i32 {
+        (self.x - other.x).abs() + (self.y - other.y).abs()
+    }
+}
+
+struct Wire {
+    position: Position,
+    steps: i32,
+    visited_at: HashMap<Position, i32>,
+}
+
+impl Wire {
+    fn new() -> Wire {
+        Wire {
+            position: Position { x: 0, y: 0 },
+            visited_at: HashMap::new(),
+            steps: 0,
+        }
+    }
+
+    fn move_right(&mut self) {
+        self.position.x += 1;
+        self.steps += 1;
+        self.visited_at.insert(self.position, self.steps);
+    }
+
+    fn move_left(&mut self) {
+        self.position.x -= 1;
+        self.steps += 1;
+        self.visited_at.insert(self.position, self.steps);
+    }
+
+    fn move_up(&mut self) {
+        self.position.y += 1;
+        self.steps += 1;
+        self.visited_at.insert(self.position, self.steps);
+    }
+
+    fn move_down(&mut self) {
+        self.position.y -= 1;
+        self.steps += 1;
+        self.visited_at.insert(self.position, self.steps);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_calc_distance() {
+        let first = Position { x: 100, y: 100 };
+        let second = Position { x: 0, y: 0 };
+
+        assert_eq!(first.distance_from(&second), 200);
+    }
+
+    //19031 too high
+    #[test]
+    fn should_solve_part_one() {
+        let input: String = fs::read_to_string(PATH).expect("");
+
+        assert_eq!(solve_part_one(&input), 1983);
+    }
+
+    #[test]
+    fn should_solve_part_two() {
+        let input: String = fs::read_to_string(PATH).expect("");
+
+        assert_eq!(solve_part_two(&input), 107754);
+    }
+}
---
+++ src/day4.rs
@@ -0,0 +1,124 @@
+use std::{fs, ops::Range};
+
+const PATH: &str = "src/day4.txt";
+
+pub fn solve() -> () {
+    let input: String = fs::read_to_string(PATH).expect("");
+    println!(
+        "Day 4 a) : {} b) : {}",
+        solve_part_one(&input),
+        solve_part_two(&input)
+    );
+}
+
+fn solve_part_one(input: &str) -> i32 {
+    parse_range(input)
+        .filter(|r| is_increasing(r) && matches_adjacent(r))
+        .count()
+        .try_into()
+        .unwrap()
+}
+
+fn solve_part_two(input: &str) -> i32 {
+    parse_range(input)
+        .filter(|r| is_increasing(r) && matches_only_adjacent(r))
+        .count()
+        .try_into()
+        .unwrap()
+}
+
+fn parse_range(input: &str) -> Range<i32> {
+    let input: Vec<i32> = input
+        .split("-")
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+
+    input[0]..input[1]
+}
+
+fn to_digits(input: &i32) -> Vec<u32> {
+    input
+        .to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect()
+}
+
+fn count_digit(digits: &Vec<u32>, digit: u32) -> u32 {
+    digits
+        .iter()
+        .filter(|d| **d == digit)
+        .count()
+        .try_into()
+        .unwrap()
+}
+
+fn is_increasing(input: &i32) -> bool {
+    to_digits(input).windows(2).all(|w| w[0] <= w[1])
+}
+
+fn matches_adjacent(input: &i32) -> bool {
+    to_digits(input).windows(2).any(|w| w[0] == w[1])
+}
+
+fn matches_only_adjacent(input: &i32) -> bool {
+    let digits = to_digits(input);
+    digits
+        .windows(2)
+        .any(|w| w[0] == w[1] && count_digit(&digits, w[0]) == 2)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_count_digit_in_digits() {
+        assert_eq!(count_digit(&vec![1, 2, 3, 4, 5, 6], 1), 1);
+        assert_eq!(count_digit(&vec![3, 3, 3, 3, 3, 3], 3), 6);
+        assert_eq!(count_digit(&vec![1, 2, 3, 4, 5, 0], 9), 0);
+    }
+
+    #[test]
+    fn should_split_into_digits() {
+        assert_eq!(to_digits(&123456), vec![1, 2, 3, 4, 5, 6]);
+        assert_eq!(to_digits(&333333), vec![3, 3, 3, 3, 3, 3]);
+        assert_eq!(to_digits(&123450), vec![1, 2, 3, 4, 5, 0]);
+    }
+
+    #[test]
+    fn should_check_for_increasing_digits() {
+        assert_eq!(is_increasing(&123456), true);
+        assert_eq!(is_increasing(&333333), true);
+        assert_eq!(is_increasing(&123450), false);
+        assert_eq!(is_increasing(&555725), false);
+    }
+
+    #[test]
+    fn should_check_for_matching_adjacent_digits() {
+        assert_eq!(matches_adjacent(&123456), false);
+        assert_eq!(matches_adjacent(&333333), true);
+        assert_eq!(matches_adjacent(&123455), true);
+    }
+
+    #[test]
+    fn should_check_for_exactly_matching_adjacent_digits() {
+        assert_eq!(matches_only_adjacent(&123456), false);
+        assert_eq!(matches_only_adjacent(&333333), false);
+        assert_eq!(matches_only_adjacent(&123455), true);
+    }
+
+    #[test]
+    fn should_solve_part_one() {
+        let input: String = fs::read_to_string(PATH).expect("");
+
+        assert_eq!(solve_part_one(&input), 2779);
+    }
+
+    #[test]
+    fn should_solve_part_two() {
+        let input: String = fs::read_to_string(PATH).expect("");
+
+        assert_eq!(solve_part_two(&input), 1972);
+    }
+}
